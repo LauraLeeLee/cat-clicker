793787604dcc35c0be07dd85ccec4304fc8ca094
Laura Logan <laurardh@aol.com>
2017-09-14 10:58:35 -0400

    removes commented out code adds one tempImg.jpg

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//temp dummy image on page load
+var tempImg = 'images/tempImg.jpg';
+
 //array for crazy lady cat collection
 var cats = [{
   img: "images/catclicker.jpg",
@@ -26,121 +29,68 @@ var cats = [{
   id:5
 }];
 
+//currently selected cat, changes when mae is clicked
+var currentCatIndex = 0;
+
+//cache page elements
+var catImg = document.getElementById('cat-image');
+var countElement = document.getElementById('count');
+var catName = document.getElementById('name');
+
+//update the page elements when a new cat is selectedCat
+function updatePage(cat) {
+  catName.innerHTML = cat.name;
+  catImg.setAttribute('src', cat.img);
+}
+
+//set up click handler
+function assignClickHandler(liElement, currentCat) {
+  liElement.addEventListener('click', function(){
+    //adds cat name to document
+    document.getElementById('name').innerHTML = currentCat.name;
+    //adds current Cat image
+    document.getElementById('cat-image').setAttribute('src',currentCat.img);
+    console.log('clicked!', currentCat.name, currentCat.img);
+    //TODO update counter and....
+    //???
+  });
+};
+
 //create list of cat names
-function createNameLi () {
+function createCatList () {
   for(var i = 0; i < cats.length; i++){
     var li = document.createElement('li');
     var catListName = cats[i].name;
     li.innerHTML = catListName;
     document.getElementById('cat-list').appendChild(li);
-    displayCurrentCat(li, cats[i]);
+     assignClickHandler(li, cats[i]);
   };
 };
-createNameLi(cats);
 
-//creats click handler
-function displayCurrentCat (liElement, currentCat) {
-  liElement.addEventListener('click', function(){
-    //adds cat name to document
-    var catName = document.createElement('h3');
-    catName.innerHTML = currentCat.name;
-    document.getElementById('name').innerHTML = catName.innerHTML;
-    //adds current Cat image
-    document.getElementById('cat-image').setAttribute('src', currentCat.img);
-    console.log(currentCat.img);
-    });
-};
-
-
-
-function catCountIncrement (catImg, counter){
+//initialize function to run on page load
+function init() {
+  createCatList(cats);
+  //set up image click handler
+  //set up the cat image click handler
   catImg.addEventListener('click', function(){
-    console.log("CurrentCat is " + cats.name);
-    console.log("Tally= " + cats.count);
-    console.log(cats.count);
-    //var selectedCat = document.getElementById('cat-image').setAttribute('src', selectedCat.img);
-    //selectedCat.src = catListImg;
-    this.count++;
-    document.getElementById('count').innerHTML = new Number(catImg.count);
+    console.log('Image clicked! Current cat index is...', currentCatIndex);
+
+    //logic??
   });
-};
-var imgElement  = document.getElementById('cat-image');
-var countElement = document.getElementById('count');
-catCountIncrement(imgElement, countElement);
+  //render the initial cat info to the page
+  updatePage(cats[currentCatIndex]);
+}
+init();
 
 
 
 /*
-var catCount = cats.count;
 
 var catDisplay = function (){
-  //creates div to hold each "cat block"(name, image, counter)
-  var catElem = document.createElement('div');
-  catElem.className = ("catElem");
-
-  //adds cat name to document
-  var catName = document.createElement('h3');
-  catName.innerHTML = cats.name;
-  catElem.appendChild(catName);
-
   //adds counter to document
   var catCounter = document.createElement('h4');
   catCounter.id = "counter-" + cats.id;
   catCounter.innerHTML = cats.count;
   catElem.appendChild(catCounter);
-
-  //adds cat image to document
-  var catImage = document.createElement('img');
-  catImage.src = cats.img;
-  catImage.style.width = '200px';
-  catImage.className = ("catImg");
-  catElem.appendChild(catImage);
-  catImage.id = "image-" + cats.id;
-  document.body.appendChild(catElem);
 };
 */
-
-/*
-function addCats(cats) {
-  for(var i = 0; i < cats.length; i++) {
-    //creates div to hold each "cat block"(name, image, counter)
-    var catElem = document.createElement('div');
-    catElem.className = ("catElem");
-    //adds cat name to document
-    var catName = document.createElement('h3');
-    catName.innerHTML = cats[i].name;
-    catElem.appendChild(catName);
-
-    //adds cat image to document
-    var catImage = document.createElement('img');
-    catImage.src = cats[i].img;
-    catImage.style.width = '200px';
-    catImage.className = ("catImg");
-    catElem.appendChild(catImage);
-    catImage.id = "image-" + cats[i].id;
-
-    //adds counter to document
-    var catCounter = document.createElement('h4');
-    catCounter.id = "counter-" + cats[i].id;
-    catCounter.innerHTML = cats[i].count;
-    catElem.appendChild(catCounter);
-
-    // use closure principal to "save" the current references of catImage & catCounter before
-    // the next loop iteration redefines them
-    // this time we are just defining a function, not even bothering to give it a name because
-    // we don't plan to reuse it, AND then invoking it immediateley, passing in what we want to preserve
-    (function makeClickHandler(listName, counterElem){
-      listName.addEventListener('click', function(){
-        console.log("catListName" , listName);
-        console.log("catCounter id #", counterElem);
-        counterElem.innerHTML = Number(counterElem.innerHTML)+1;
-        var selectedCat = document.createElement('img');
-        selectedCat.src = catListImg;
-        selectedCat.appendChild(body);
-
-      });
-    })(catImage, catCounter)
-    document.body.appendChild(catElem);
-  };
-};   */
-//addCats(cats);
