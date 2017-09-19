2d674a95fcc341dd99c0babdf73bc172cc9988ce
Laura Logan <laurardh@aol.com>
2017-09-19 10:47:41 -0400

    creates octopus branch, adds model for MVO

--- js/script.js
+++ js/script.js
@@ -1,38 +1,59 @@
-//temp dummy image on page load
-var tempImg = 'images/tempImg.jpg';
 
-//array for crazy lady cat collection
-var cats = [{
-  img: "images/catclicker.jpg",
-  name: "Kitty",
-  count: 0,
-  id: 1
-},
-{ img: "images/catclicker2.jpg",
-  name: "Mr. Tipps",
-  count: 0,
-  id: 2
-},
-{ img: "images/catclicker3.jpg",
-  name: "FraidyCat",
-  count: 0,
-  id: 3
-},
-{ img: "images/catclicker4.jpg",
-  name: "MsClawz",
-  count: 0,
-  id: 4
-},
-{ img: "images/catclicker5.jpg",
-  name: "ZsaZsa",
-  count: 0,
-  id:5
-}];
+//--------------Model
+  var model = {
+     currentCat: null,
+     currentCatIndex: 0,
+     currentCat: cats.img,
+     currentCount: cats.count,
+    //array for crazy lady cat collection
+      cats: [
+      { img: "images/catclicker.jpg",
+        name: "Kitty",
+        count: 0,
+        id: 1
+      },
+      { img: "images/catclicker2.jpg",
+        name: "Mr. Tipps",
+        count: 0,
+        id: 2
+      },
+      { img: "images/catclicker3.jpg",
+        name: "FraidyCat",
+        count: 0,
+        id: 3
+      },
+      { img: "images/catclicker4.jpg",
+        name: "MsClawz",
+        count: 0,
+        id: 4
+      },
+      { img: "images/catclicker5.jpg",
+        name: "ZsaZsa",
+        count: 0,
+        id:5
+      }];
+  };
+
+
+//----------------Octopus
+  var octopus = {
+    init: function() {
+      //sets current cat to the first cat
+      model.currentCat = model.
+    }
+
+  },
+
+
+//--------------View
+
+
+
+
+
 
 //currently selected cat, changes when mae is clicked
-var currentCatIndex = 0;
-var currentCat = cats.img;
-var currentCount = cats.count;
+
 
 //cache page elements
 var catImg = document.getElementById('cat-image');
